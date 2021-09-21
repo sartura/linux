89d0b452e426aa9e473a32d986b1134a670bccbc
Robert Marko <robert.marko@sartura.hr>
2021-09-21 17:35:52 +0200

    net: marvell: prestera: remove vid_begin -> vid_end range from VLAN objects
    Apply upstream commits:
    net: switchdev: remove vid_begin -> vid_end range from VLAN objects
    b7a9e0da2d1c954b7c38217a29e002528b90d174
    and
    net: marvell: prestera: fix uninitialized vid in prestera_port_vlans_add
    c612fe7808033ca2c7000fa06a4abb12a5e3e253
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- drivers/net/ethernet/marvell/prestera/prestera_switchdev.c
+++ drivers/net/ethernet/marvell/prestera/prestera_switchdev.c
@@ -384,7 +384,7 @@ static int mvsw_pr_port_vlans_add(struct prestera_port *port,
 	struct prestera_bridge_port *br_port;
 	struct prestera_bridge_device *bridge_device;
 	struct prestera_switch *sw = port->sw;
-	u16 vid;
+	int err;
 
 	if (netif_is_bridge_master(orig_dev))
 		return 0;
@@ -400,15 +400,11 @@ static int mvsw_pr_port_vlans_add(struct prestera_port *port,
 	if (!bridge_device->vlan_enabled)
 		return 0;
 
-	for (vid = vlan->vid_begin; vid <= vlan->vid_end; vid++) {
-		int err;
-
-		err = mvsw_pr_bridge_port_vlan_add(port, br_port,
-						   vid, flag_untagged,
-						   flag_pvid, extack);
-		if (err)
-			return err;
-	}
+	err = mvsw_pr_bridge_port_vlan_add(port, br_port,
+					   vlan->vid, flag_untagged,
+					   flag_pvid, extack);
+	if (err)
+		return err;
 
 	if (list_is_singular(&bridge_device->port_list))
 		prestera_rif_enable(port->sw, bridge_device->dev, true);
@@ -459,7 +455,6 @@ static int mvsw_pr_port_vlans_del(struct prestera_port *port,
 	struct prestera_switch *sw = port->sw;
 	struct net_device *orig_dev = vlan->obj.orig_dev;
 	struct prestera_bridge_port *br_port;
-	u16 vid;
 
 	if (netif_is_bridge_master(orig_dev))
 		return -EOPNOTSUPP;
@@ -471,8 +466,7 @@ static int mvsw_pr_port_vlans_del(struct prestera_port *port,
 	if (!br_port->bridge_device->vlan_enabled)
 		return 0;
 
-	for (vid = vlan->vid_begin; vid <= vlan->vid_end; vid++)
-		mvsw_pr_bridge_port_vlan_del(port, br_port, vid);
+	mvsw_pr_bridge_port_vlan_del(port, br_port, vlan->vid);
 
 	return 0;
 }
